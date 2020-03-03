1b2a52186027a3ec490321751945d62fb750e039
Filip <filip.janowski@gmail.com>
2020-03-03 16:01:35 +0100

    gunicorn, nginx

--- django_test/settings.py
+++ django_test/settings.py
@@ -141,8 +141,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
-STATIC_URL = '/static/'
-STATIC_ROOT = r'C:\Users\Filip\PycharmProjects\django_test\static'
+STATIC_URL = "/staticfiles/"
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 
 CORS_ORIGIN_WHITELIST = ['http://localhost:3000']
 
